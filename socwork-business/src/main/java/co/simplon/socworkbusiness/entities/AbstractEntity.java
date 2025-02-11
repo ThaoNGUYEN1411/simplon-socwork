ae5326945b6a696701d598f7e2feff197050b6c1
Thao NGUYEN <phuongthaolyon@gmail.com>
2025-02-11 19:42:41 +0100

    update repos

--- socwork-business/src/main/java/co/simplon/socworkbusiness/entities/AbstractEntity.java
+++ socwork-business/src/main/java/co/simplon/socworkbusiness/entities/AbstractEntity.java
@@ -10,9 +10,9 @@ abstract class AbstractEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
-    public long getId() {
+    public Long getId() {
 	return id;
     }
 
